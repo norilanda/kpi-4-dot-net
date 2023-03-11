26183b158ca540d0eeff7e42763c322800de0dae
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2023-03-11 19:30:29 +0200

    delete BookOfAuthor class, add list of books to class author and list of authors to class book, change library members

--- laba1/laba1/Author.cs
+++ laba1/laba1/Author.cs
@@ -10,14 +10,38 @@ namespace laba1
     {
         private string _firstName;
         private string _lastName;
+        private List<Book> _bookList;
 
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        public string Firstname => this._firstName;
+        public string Lastname => this._lastName;
+        public List<Book> BookList => _bookList;
 
         public Author(string firstName, string lastName)
         {
             this._firstName = firstName;
             this._lastName = lastName;
+            this._bookList = new List<Book>();
+        }
+        public void AddBook(Book book) => this._bookList.Add(book);
+        public static List<Author> CreateAuthorList()
+        {
+            List<Author> authorList = new List<Author>()
+            {
+                new Author("Ray", "Bradbury"),
+                new Author("Jack", "London"),
+                new Author("Joseph", "Conrad"),
+                new Author("Jane", "Austen"),
+                new Author("Scott", "Fitzgerald"),
+                new Author("Harper", "Lee"),
+                new Author("George", "Orwell"),
+                new Author("Ernest", "Hemingway"),
+                new Author("Stephen", "Hawkingon"),
+                new Author("Leonard", "Mlodinow"),
+                new Author("Markus", "Zusak"),
+                new Author("J. R. R.", "Tolkien"),
+                new Author("Stieg", "Larsson")
+            };
+            return authorList;
         }
     }
 }

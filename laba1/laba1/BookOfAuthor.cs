24c7eca78e48aed3109487ab4ffc592286fdf0f8
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-03-17 09:08:48 +0200

    Revert "add creating books and their authors"
    This reverts commit c38b9d0305a702b2dab351ac2205a71c59c4847a.

---
+++ laba1/laba1/BookOfAuthor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    public class BookOfAuthor
+    {
+        private Book _book;
+        private Author _author;
+
+        public Book book { get; set; }
+        public Author author { get; set; }
+
+        public BookOfAuthor(Book book, Author author)
+        {
+            this._book = book;
+            this._author = author;
+        }
+    }
+}

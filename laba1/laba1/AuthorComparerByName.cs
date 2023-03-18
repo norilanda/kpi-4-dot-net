b0be64d519e387c9655ed7535ab084027afba75f
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-03-18 17:18:46 +0200

    add author comparer by first and last name

---
+++ laba1/laba1/AuthorComparerByName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    public class AuthorComparerByName : IEqualityComparer<Author>
+    {
+        public bool Equals(Author x, Author y)
+        {
+            return x.Firstname.Equals(y.Firstname) && x.Lastname.Equals(y.Lastname);
+        }
+        public int GetHashCode(Author obj)
+        {
+            if (obj == null) return 0;
+            return (obj.Firstname+obj.Lastname).GetHashCode();
+        }
+    }
+}

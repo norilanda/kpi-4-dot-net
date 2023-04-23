c9bc39426df724d7aa8c917aee53194ede976a7d
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-04-23 22:19:25 +0300

    add input/output module

---
+++ laba3/laba3/InputOutput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3
+{
+    public static class InputOutput
+    {
+        public static void DisplayEvents(List<Event> events)
+        {
+            Console.WriteLine("N  LEVEL\t SOURCE \tTIME\n=======================================================");
+            for (int i = 0; i < events.Count(); i++)
+            {
+                Console.Write($"{i+1}. ");
+                Console.WriteLine(events[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}

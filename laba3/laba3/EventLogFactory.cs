fda51a6d865efad5f1e784a6a6efca060b45a86b
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-04-24 13:33:46 +0300

    add event log factory

---
+++ laba3/laba3/EventLogFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using laba3.EventLogs;
+
+namespace laba3
+{
+    public static class EventLogFactory
+    {
+        public static EventLog Create(string fileName)
+        {
+            string extention = Path.GetExtension(fileName);
+            switch (extention)
+            {
+                case ".txt": return new TextEventLog();
+                case ".xml": return new XmlEventLog();
+                default: throw new ArgumentException("file type cannot be processed");
+            }
+        }
+    }
+}

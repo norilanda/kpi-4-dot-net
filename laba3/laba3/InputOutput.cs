d743963212e59156f40700abd07bfb849c1f269c
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-04-24 13:25:09 +0300

    add GetUserInput function to InputOutput

--- laba3/laba3/InputOutput.cs
+++ laba3/laba3/InputOutput.cs
@@ -18,5 +18,34 @@ namespace laba3
                 Console.WriteLine();
             }
         }
+        public static void GetUserInput(out string fileName, out int eventNumberToGenerate, out bool shouldCreateNewFile)
+        {
+            Console.WriteLine("Enter file name with extension (Ex.: file.txt)");
+            fileName = Console.ReadLine();
+            while (Path.GetExtension(fileName) == String.Empty)
+            {
+                Console.WriteLine("Your file should have an extension specified! Try again!");
+                fileName = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter number of events to generate (Ex.: 3)");
+            eventNumberToGenerate = -1;
+            while (!int.TryParse(Console.ReadLine(), out eventNumberToGenerate) || eventNumberToGenerate <= 0) 
+            {
+                Console.WriteLine("Please, enter correct positive number!");
+            }
+
+            Console.Write("Should the new log file be created or use existing one? n - new, e - existing: ");
+            string newOrExisting = Console.ReadLine().ToLower().TrimStart();
+            while (newOrExisting.Length == 0 || (newOrExisting[0] != 'n' && newOrExisting[0] != 'e'))
+            {
+                Console.WriteLine("Type 'n' or 'e'!");
+                newOrExisting = Console.ReadLine().ToLower().TrimStart(); 
+            }
+            if (newOrExisting[0] == 'n')
+                shouldCreateNewFile = true;
+            else
+                shouldCreateNewFile = false;
+        }
     }
 }

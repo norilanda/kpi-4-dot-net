4665a4d52f3faa1a5f97607fe6fb7c346ddc737d
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-04-24 13:34:52 +0300

    add creating eventLog object and processing file in Main

--- laba3/laba3/Program.cs
+++ laba3/laba3/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace laba3
+﻿using laba3.EventLogs;
+namespace laba3
 {
 //    4) Реалізувати задачу протоколювання подій.Журнал подій може мати декілька форматів, котрий задається з самого початку (наприклад,
 //текстовий файл, XML, тощо). Події, котрі надсилаються в журнал повинні мати наступні характеристики:
@@ -11,7 +12,31 @@
     {
         static void Main(string[] args)
         {
+            string fileName = "file.txt";
+            int eventNumberToGenerate = 2;
+            bool shouldCreateNewFile = false;
+            InputOutput.GetUserInput(out fileName, out eventNumberToGenerate, out shouldCreateNewFile);
+            try
+            {
+                EventLog eventLog = EventLogFactory.Create(fileName);
+                List<Event> events = EventGenerator.Generate(eventNumberToGenerate);
 
+                if (shouldCreateNewFile)
+                    eventLog.CreateEventLog(fileName, events);
+                else
+                    eventLog.UpdateEventLog(fileName, events);
+                List<Event> eventsFromFile = eventLog.Load(fileName);
+
+                InputOutput.DisplayEvents(eventsFromFile);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FileNotFoundException )
+            {
+                Console.WriteLine($"File {fileName} could not be found");
+            }
         }
     }
 }

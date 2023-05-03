927270146478eb8d83f4493061a5ea57a8564dad
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-05-03 21:04:52 +0300

    add expression parser

---
+++ laba4/laba4/ExpressionParser.cs
@@ -0,0 +1,82 @@
+﻿using laba4.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace laba4
+{
+    /// <summary>
+    ///     Creates Expression from string
+    /// </summary>
+    public static class ExpressionParser
+    {
+        public static IExpression CreateExpression(string expression)
+        {
+            expression = expression.Replace(" ", "");
+            try
+            {
+                return CreateExpressionRecursion(expression, 0, expression.Length - 1);
+            }
+            catch (ArgumentException) { throw; }
+        }
+        private static IExpression CreateExpressionRecursion(string str, int firstPos, int lastPos)
+        {
+            string delims = "/*+-";
+            string sub = str.Substring(firstPos, lastPos - firstPos + 1);
+            try
+            {
+                if (sub.IndexOfAny(delims.ToCharArray()) == -1)
+                    return CreateSimpleExpression(sub);
+                int pos = 0;
+                int prior, minPrior = 10;
+
+                for (int i = firstPos; i <= lastPos; i++)
+                {
+                    switch (str[i])
+                    {
+                        case '+':
+                            prior = 1;
+                            break;
+                        case '-':
+                            prior = 2;
+                            break;
+                        case '/':
+                        case '*':
+                            prior = 3;
+                            break;
+                        default: prior = 10; break;
+                    }
+                    if (prior < minPrior)
+                    {
+                        minPrior = prior;
+                        pos = i;
+                    }
+                }
+                //if (minPrior == 10) 
+                //    throw new ArgumentException("Expression cannot processed!");
+                ComplexExpression expr = new ComplexExpression();
+                expr.OperationSign = str[pos];
+                expr.LeftExpression = CreateExpressionRecursion(str, firstPos, pos - 1);
+                expr.RightExpression = CreateExpressionRecursion(str, pos + 1, lastPos);
+                return expr;
+            }
+            catch (ArgumentException) { throw; }
+        }
+        private static IExpression CreateSimpleExpression(string expression)
+        {
+            try
+            {
+                if (expression == string.Empty)
+                    throw new ArgumentException("Expression cannot be processed!");
+                int number;
+                if (!int.TryParse(expression, out number))
+                    return new Variable(expression);
+                return new Constant(number);
+            }
+            catch (ArgumentException) { throw; }
+        }
+    }
+}
--- laba4/laba4/Program.cs
+++ laba4/laba4/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace laba4
+﻿using laba4.Expressions;
+
+namespace laba4
 {
 //  7) Реалізувати дерево розбору виразу, заданого наведеними нижче синтаксичними правилами:
 //  <вираз>::=<простий вираз> | <складний вираз>
@@ -11,7 +13,19 @@
     {
         static void Main(string[] args)
         {
-            
+            string str = "4+x-9";
+            Dictionary<string,double> parameters = new Dictionary<string,double>();
+            parameters["x"] = 1;
+            try
+            {
+                IExpression expr = ExpressionParser.CreateExpression(str);
+                string composedExpression = expr.ComposeExpression();
+                double exprResult = expr.Calculate(parameters);
+            } 
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }

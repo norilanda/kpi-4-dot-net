1ad855bc924406d23a0ac7510e081992503990e4
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-05-17 10:37:43 +0300

    add factory methods for search and sort algorithms

---
+++ laba5/laba5/Search/ChooseSearchAlgo.cs
@@ -0,0 +1,28 @@
+﻿using laba5.Search.SearchAlgo;
+using laba5.Sort.SortAlgo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba5.Search
+{
+    public static class ChooseSearchAlgo
+    {
+        public enum SearchMehod
+        {
+            Simple,
+            Merge,
+        }
+        public static SearchingAlgorithm Create(SearchMehod method)
+        {
+            switch (method)
+            {
+                case SearchMehod.Simple: return new SimpleSearch();
+                default:
+                case SearchMehod.Merge: return new MergeSearch();
+            }
+        }
+    }
+}
---
+++ laba5/laba5/Sort/ChooseSortAlgo.cs
@@ -0,0 +1,27 @@
+﻿using laba5.Sort.SortAlgo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba5.Sort
+{
+    public static class ChooseSortAlgo
+    {
+        public enum SortMehod
+        {
+            Buble,
+            Merge,
+        }
+        public static SortingAlgorithm Create(SortMehod method)
+        {
+            switch (method)
+            {
+                case SortMehod.Buble: return new BubleSort();
+                default:
+                case SortMehod.Merge: return new MergeSort();
+            }
+        }
+    }
+}
